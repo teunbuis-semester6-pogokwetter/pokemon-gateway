aafa5959988f6ef3cf9733c730b41774d2489a8e
Teun <teunbuis17@gmail.com>
2023-06-13 21:51:41 +0200

    Update AuthenticationSuccessHandler.java

--- src/main/java/com/teun/pokemongateway/config/AuthenticationSuccessHandler.java
+++ src/main/java/com/teun/pokemongateway/config/AuthenticationSuccessHandler.java
@@ -2,6 +2,8 @@ package com.teun.pokemongateway.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -11,9 +13,12 @@ import java.io.IOException;
 @Component
 public class AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationSuccessHandler.class);
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
-        response.sendRedirect("/home/welcome");
+        logger.info("Successfully logged in: " + authentication.getName());
+//        response.sendRedirect("/home/welcome");
     }
 }
